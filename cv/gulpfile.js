244df4804349ed058a2afd6830755563d2c70a39
Constantin Melniciuc <constantin@etergo.com>
2020-03-31 13:55:12 +0200

    Updated Package and overall the cv feel and typings

--- cv/gulpfile.js
+++ cv/gulpfile.js
@@ -1,45 +1,51 @@
 // The input SCSS files and the SCSS output path
-var scssInput = [
-	'app/scss/style.scss',
-	'app/scss/print.scss'
-	];
+var scssInput = 'app/scss/**/*.scss';
 var scssOutput = 'app/css';
 
 // Start everything up.
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const { src, dest, watch } = require('gulp');
+
+const sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
 
-// Watch SASS.
-gulp.task('sass', function() {
-  return gulp
-    .src(scssInput)
-    .pipe(sourcemaps.init())
+
+function css() {
+  return src(scssInput)
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(autoprefixer())
-    .pipe(sourcemaps.write())
-
-    .pipe(gulp.dest(scssOutput))
+    .pipe(dest(scssOutput))
     .pipe(browserSync.reload({
       stream: true
     }))
-});
+}
 
 // Spin up server and reload.
-gulp.task('browserSync', function() {
+function bsync() {
   browserSync.init({
     server: {
       baseDir: './'
     },
   })
-})
+}
+
+function js() {
+  return src('app/js/*.js', { sourcemaps: true })
+    .pipe(concat('app.min.js'))
+    .pipe(dest('build/js', { sourcemaps: true }))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+}
 
 
-gulp.task('watch', ['browserSync', 'sass'], function (){
-  gulp.watch('app/scss/**/*.scss', ['sass']);
-  gulp.watch('./*.html', browserSync.reload);
-  gulp.watch('app/js/**/*.js', browserSync.reload);
-});
+exports.bsync = bsync;
+exports.js = js;
+exports.css = css;
+exports.default = function () {
+  bsync();
+  watch(scssInput, css);
+  watch('app/js/**/*.js', js);
+}
