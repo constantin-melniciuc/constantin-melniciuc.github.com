4554e1f2dcfbe864e93978b5376de88d3c5e40a5
Constantin Melniciuc <costea22@gmail.com>
2022-01-20 14:52:27 +0100

    Updated cv

--- cv/gulpfile.js
+++ cv/gulpfile.js
@@ -1,45 +1,46 @@
 // The input SCSS files and the SCSS output path
-var scssInput = 'app/scss/**/*.scss';
-var scssOutput = 'app/css';
+var scssInput = "app/scss/**/*.scss";
+var scssOutput = "app/css";
 
 // Start everything up.
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch } = require("gulp");
 
-const sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-
-
+const sass = require("gulp-sass")(require("node-sass"));
+var browserSync = require("browser-sync").create();
+var sourcemaps = require("gulp-sourcemaps");
+var autoprefixer = require("gulp-autoprefixer");
 
 function css() {
   return src(scssInput)
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(autoprefixer())
     .pipe(dest(scssOutput))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    );
 }
 
 // Spin up server and reload.
 function bsync() {
   browserSync.init({
     server: {
-      baseDir: './'
+      baseDir: "./",
     },
-  })
+  });
 }
 
 function js() {
-  return src('app/js/*.js', { sourcemaps: true })
-    .pipe(concat('app.min.js'))
-    .pipe(dest('build/js', { sourcemaps: true }))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+  return src("app/js/*.js", { sourcemaps: true })
+    .pipe(concat("app.min.js"))
+    .pipe(dest("build/js", { sourcemaps: true }))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    );
 }
-
 
 exports.bsync = bsync;
 exports.js = js;
@@ -47,5 +48,5 @@ exports.css = css;
 exports.default = function () {
   bsync();
   watch(scssInput, css);
-  watch('app/js/**/*.js', js);
-}
+  watch("app/js/**/*.js", js);
+};
